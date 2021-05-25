9ef79969e4dfbf8d133ff0534bfac2fa03443af7
JSourianos <thomasourianos@live.no>
2021-05-25 09:36:28 +0200

    created home and login screen, and homescreenbutton component

---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {View, Text} from "react-native-web";
+
+function LoginScreen() {
+    return (
+        <View>
+            <Text>LoginScreen</Text>
+        </View>
+    );
+}
