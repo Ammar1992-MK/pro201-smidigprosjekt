d22c44038ea4d34cd06b150948be5d22ab98050e
JSourianos <thomasourianos@live.no>
2021-05-25 10:10:22 +0200

    added stack navigator

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,7 +1,7 @@
 import React from "react";
-import {View, Text} from "react-native-web";
+import {View, Text} from "react-native";
 
-function LoginScreen() {
+export default function LoginScreen() {
     return (
         <View>
             <Text>LoginScreen</Text>
