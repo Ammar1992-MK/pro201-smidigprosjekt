6e93134f7a4eaf11650e84379f80eca608ddcd31
vetlehlu <56083515+vetlehlu@users.noreply.github.com>
2021-05-31 11:59:06 +0200

    Changed buttons on Learn-screen

--- src/components/HomeScreenButton.js
+++ src/components/HomeScreenButton.js
@@ -1,71 +1,71 @@
-import React from "react";
-import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import React from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 
 export const HomeScreenButton = (props) => {
-  const add = require("../../assets/icons/add.png");
-  const search = require("../../assets/icons/search.png");
-  const upload = require("../../assets/icons/file_upload.png");
-  const school = require("../../assets/icons/school.png");
-  let icon;
+	const add = require('../../assets/icons/add.png');
+	const search = require('../../assets/icons/search.png');
+	const upload = require('../../assets/icons/file_upload.png');
+	const school = require('../../assets/icons/school.png');
+	let icon;
 
-  switch (props.icon) {
-    case "add":
-      icon = add;
-      break;
-    case "search":
-      icon = search;
-      break;
-    case "school":
-      icon = school;
-      break;
-    case "upload":
-      icon = upload;
-      break;
-  }
+	switch (props.icon) {
+		case 'add':
+			icon = add;
+			break;
+		case 'search':
+			icon = search;
+			break;
+		case 'school':
+			icon = school;
+			break;
+		case 'upload':
+			icon = upload;
+			break;
+	}
 
-  return (
-    <TouchableOpacity
-      style={HomeScreenButtonStyle.container}
-      onPress={props.onPress}
-    >
-      <View style={HomeScreenButtonStyle.button}>
-        <View style={HomeScreenButtonStyle.innerContainer}>
-          <Image style={HomeScreenButtonStyle.icon} source={icon} />
-          <Text style={HomeScreenButtonStyle.title}>{props.title}</Text>
-        </View>
-      </View>
-    </TouchableOpacity>
-  );
+	return (
+		<TouchableOpacity
+			style={HomeScreenButtonStyle.container}
+			onPress={props.onPress}
+		>
+			<View style={HomeScreenButtonStyle.button}>
+				<View style={HomeScreenButtonStyle.innerContainer}>
+					<Image style={HomeScreenButtonStyle.icon} source={icon} />
+					<Text style={HomeScreenButtonStyle.title}>{props.title}</Text>
+				</View>
+			</View>
+		</TouchableOpacity>
+	);
 };
 
 const HomeScreenButtonStyle = StyleSheet.create({
-  container: {
-    width: "73%",
-    height: "13.5%",
-    margin: 20,
-  },
-  button: {
-    width: "100%",
-    height: "100%",
-    justifyContent: "center",
-    backgroundColor: "#B7D381",
-    borderRadius: 8,
-    elevation: 6
-  },
-  innerContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    width: "80%",
-    marginLeft: "10%",
-  },
-  title: {
-    fontFamily: 'ArialBold',
-    fontSize: 27,
-    color: "#174A5B",
-    marginLeft: 40
-  },
-  icon: {
-    width: 64,
-    height: 64,
-  },
+	container: {
+		width: '73%',
+		height: '13.5%',
+		margin: 20,
+	},
+	button: {
+		width: '100%',
+		height: '100%',
+		justifyContent: 'center',
+		backgroundColor: '#B7D381',
+		borderRadius: 8,
+		elevation: 6,
+	},
+	innerContainer: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		width: '80%',
+		marginLeft: '10%',
+	},
+	title: {
+		fontFamily: 'ArialBold',
+		fontSize: 27,
+		color: '#174A5B',
+		marginLeft: 40,
+	},
+	icon: {
+		width: 64,
+		height: 64,
+	},
 });
